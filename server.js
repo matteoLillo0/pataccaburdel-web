8e2a1202052b73972b14fa27eb95b464ead75f2d
matteo <matteo.angiolilo77@gmail.com>
2026-03-21 10:58:55 +0100

    initial set-up with node+docker architecture

---
+++ server.js
@@ -0,0 +1,12 @@
+// import
+const express = require('express');
+const path = require('path');
+
+const app = express(); // creates an express application
+const PORT = 3000; 
+
+app.use(express.static(path.join(__dirname, 'public'))); // serves only files in /public, manage alr path traversal errors
+
+app.listen(PORT, () => {
+    console.log('Server in ascolto sulla porta: ${PORT}')
+});
